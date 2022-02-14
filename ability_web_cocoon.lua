270d0dc2be4e2ef7e7baf680a757097d66e9b377
imgmaterial <59793640+imgmaterial@users.noreply.github.com>
2022-02-14 20:15:04 +0100

    Add files via upload

---
+++ ability_web_cocoon.lua
@@ -0,0 +1,4 @@
+ability_web_cocoon = class({})
+LinkLuaModifier( "modifer_ability_web_cocoon", "modifiers/modifer_ability_web_cocoon", LUA_MODIFIER_MOTION_NONE )
+-- shoots projectile at multiple heroes in radius abblies linked modifer 
+
